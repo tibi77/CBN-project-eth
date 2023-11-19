64f12a2f3c900b37bd006d15957a8172d3c95737
Leonard Lepadatu <leonardlepadatu@yahoo.com>
2023-11-19 07:12:30 +0200

    mai conteaza?

--- apps/cbn/pages/front/blockchain.tsx
+++ apps/cbn/pages/front/blockchain.tsx
@@ -1,13 +1,19 @@
 import FrontLayout from "../../components/FrontLayout";
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, Divider, IconButton, Typography } from "@mui/material";
 import { useAppState } from "../../stateContext";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbsDownIcon from "@mui/icons-material/ThumbDown";
+import { useEffect, useState } from "react";
 
 const Blockchain = () => {
   const { posts } = useAppState();
   const category = "Blockchain";
+  const [walletAddr, setwalletAddr] = useState("");
 
+  useEffect(() => {
+    console.log(typeof window);
+    setwalletAddr(localStorage.getItem("walletAddr") || ("" as string));
+  }, []);
   return (
     <FrontLayout>
       <h1>Blockchain</h1>
@@ -15,24 +21,52 @@ const Blockchain = () => {
         .filter((post) => post.category === category)
         .map((post) => {
           return (
-            <Box>
+            <Box
+              key={post.title}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+                width: "30em",
+                border: "1px solid black",
+                borderRadius: "1em",
+                padding: "1em",
+              }}
+            >
               <Typography>{post.title}</Typography>
-              <Typography>{post.category}</Typography>
-              <Typography>{post.date}</Typography>
-              <Typography>{post.article}</Typography>
+              <Typography variant="caption">
+                Category: {post.category} | Date: {post.date}
+              </Typography>
+              <Divider />
               <Box
                 sx={{
-                  display: "flex",
-                  flexDirection: "row",
+                  height: "10em",
+                  overflowX: "hidden",
+                  overflowY: "auto",
+                  wordWrap: "break-word",
                 }}
               >
-                <IconButton>
-                  <ThumbUpIcon />
-                </IconButton>
-                <IconButton>
-                  <ThumbsDownIcon />
-                </IconButton>
+                <Typography variant="body1">{post.article}</Typography>
               </Box>
+              <Divider />
+              {walletAddr && (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "3em",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <IconButton>
+                    <ThumbUpIcon color="secondary" />
+                  </IconButton>
+                  <IconButton>
+                    <ThumbsDownIcon color="primary" />
+                  </IconButton>
+                </Box>
+              )}
             </Box>
           );
         })}
--- apps/cbn/pages/front/world.tsx
+++ apps/cbn/pages/front/world.tsx
@@ -1,13 +1,20 @@
-import { Box, IconButton, Typography } from "@mui/material";
 import FrontLayout from "../../components/FrontLayout";
+import { Box, Divider, IconButton, Typography } from "@mui/material";
 import { useAppState } from "../../stateContext";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbsDownIcon from "@mui/icons-material/ThumbDown";
+import { useEffect, useState } from "react";
+
 
 const World = () => {
   const { posts } = useAppState();
-  const category = "World"
+  const category = "World";
+  const [walletAddr, setwalletAddr] = useState("");
 
+useEffect(() => {
+  console.log(typeof window)
+  setwalletAddr(localStorage.getItem("walletAddr") || "" as string);
+}, []);
   return (
     <FrontLayout>
       <h1>World</h1>
@@ -15,25 +22,52 @@ const World = () => {
         .filter((post) => post.category === category)
         .map((post) => {
           return (
-            <Box>
+            <Box
+              key={post.title}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+                width: "30em",
+                border: "1px solid black",
+                borderRadius: "1em",
+                padding: "1em",
+              }}
+            >
               <Typography>{post.title}</Typography>
-              <Typography>{post.category}</Typography>
-              <Typography>{post.date}</Typography>
-              <Typography>{post.article}</Typography>
+              <Typography variant="caption">
+                Category: {post.category} | Date: {post.date}
+              </Typography>
+              <Divider />
               <Box
                 sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
+                  height: "10em",
+                  overflowX: "hidden",
+                  overflowY: "auto",
+                  wordWrap: "break-word",
                 }}
               >
-                <IconButton>
-                  <ThumbUpIcon />
-                </IconButton>
-                <IconButton>
-                  <ThumbsDownIcon />
-                </IconButton>
+                <Typography variant="body1">{post.article}</Typography>
               </Box>
+              <Divider />
+              {walletAddr && (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "3em",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <IconButton>
+                    <ThumbUpIcon color="secondary" />
+                  </IconButton>
+                  <IconButton>
+                    <ThumbsDownIcon color="primary" />
+                  </IconButton>
+                </Box>
+              )}
             </Box>
           );
         })}
